4fd1b81195be60d5ba56d8ec9d08a109bdf0c9ce
Richard Alanya <richard_ac@outlook.com>
2024-08-21 22:41:45 -0500

    formulario usuario parte 1

--- views/usuarios/index.php
+++ views/usuarios/index.php
@@ -32,10 +32,64 @@
                 </table>
             </div>
             <div class="tab-pane fade mt-2" id="nuevo" role="tabpanel" aria-labelledby="nuevo-tab">
-                <div class="input-group mb-3">
-                    <span class="input-group-text" id="basic-addon1">Nombre</span>
-                    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                </div>                
+                <div class="row p-5">
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Nombres</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-list"></i></span>
+                            <input type="text" class="form-control" placeholder="Nombres">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Apellidos</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-list-alt"></i></span>
+                            <input type="text" class="form-control" placeholder="Apellidos">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Correo</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                            <input type="email" class="form-control" placeholder="Correo Electrónico">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Teléfono</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                            <input type="number" class="form-control" placeholder="Teléfono">
+                        </div>
+                    </div>
+                    <div class="col-lg-8 col-sm-6">
+                        <label>Dirección</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-home"></i></span>
+                            <input type="text" class="form-control" placeholder="Dirección">
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Clave</label>
+                        <div class="input-group mb-3">
+                            <span class="input-group-text"><i class="fas fa-lock"></i>Clave</span>
+                            <input type="password" class="form-control" placeholder="Contraseña">
+                        </div>
+                    </div>                    
+                    <div class="col-lg-4 col-sm-6">
+                        <label>Rol</label>
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupSelect01"><i class="fas fa-id-card"></i></label>
+                            <select class="form-select" id="inputGroupSelect01">
+                                <option selected>Seleccionar...</option>
+                                <option value="1">ADMINISTRADOR</option>
+                                <option value="2">VENDEDOR</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-end">
+                    <button class="btn btn-primary" type="button">Registrar</button>
+                </div>
             </div>
         </div>
     </div>
